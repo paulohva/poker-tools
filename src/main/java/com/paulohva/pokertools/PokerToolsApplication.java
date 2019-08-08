85964997d54e82812aa7100548ac7bfecb7c785e
Paulo Valadares <paulohva@gmail.com>
2019-08-08 10:52:15 +0100

    Initial commit

---
+++ src/main/java/com/paulohva/pokertools/PokerToolsApplication.java
@@ -0,0 +1,13 @@
+package com.paulohva.pokertools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulohva/pokertools/PokerToolsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.paulohva.pokertools;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PokerToolsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
